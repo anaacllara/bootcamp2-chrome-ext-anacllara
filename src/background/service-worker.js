144a964cb4536085b1ebf3968dbe66c0132e4846
anaacllara <anaclarafc09@gmail.com>
2025-09-06 17:29:19 -0300

    update manifest and adjust service-worker

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -1,8 +1,7 @@
-chrome.tabs.onUpdate.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url){
         chrome.storage.local.get(['blockedSites'], (result) => {
             const blockedSites = result.blockedSites || [];
-
             for (const site of blockedSites){
                 if(tab.url.includes(site)){
                     const blockedPageUrl = chrome.runtime.getURL('pages/blocked.html');
